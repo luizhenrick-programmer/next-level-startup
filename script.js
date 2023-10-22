3899a40472739325c473a2ebf8a5d7972e835ef4
luizhenrick-programmer <105799263+luizhenrick-programmer@users.noreply.github.com>
2023-10-22 15:44:43 -0300

    Finalizando
    Adicionando navegação com JavaScript

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var menuButton = document.getElementById('menu-burguer');
 var menuBarMobile = document.querySelector('.nav-list');
 var menuBarMobileUL = document.querySelector('.nav-list-ul');
+var menuLogoMobile = document.querySelector('.logo')
 
 function MenuShow() {
   if (menuButton.classList.contains('show')) {
     menuButton.classList.remove('show');
     menuButton.innerText = 'menu';
     menuBarMobile.style.display = 'none';
-    menuBarMobileUL.classList.remove('animation');
     
   } else {
     menuButton.classList.add('show')
     menuButton.innerText = 'cancel';
     menuBarMobile.style.display = 'block';
-    menuBarMobileUL.classList.add('animation');
+    menuLogoMobile.classList.add('menulogo')
   }
 }
 
